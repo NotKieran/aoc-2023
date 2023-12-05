064ca36edee09f9857c02eb8f5848ab608d4b326
Kieran Hall <16665502+NotKieran@users.noreply.github.com>
2023-12-05 17:09:00 +0000

    Add Day 2

--- Sources/Day02.swift
+++ Sources/Day02.swift
@@ -1,25 +1,113 @@
 import Algorithms
 
+struct Game {
+    let id: Int
+    let isValid: Bool
+    let minRed: Int
+    let minGreen: Int
+    let minBlue: Int
+    let power: Int
+}
+
 struct Day02: AdventDay {
   // Save your data in a corresponding text file in the `Data` directory.
   var data: String
 
   // Splits input data into its component parts and convert from string.
-  var entities: [[Int]] {
-    data.split(separator: "\n\n").map {
-      $0.split(separator: "\n").compactMap { Int($0) }
+    var entities: [String] {
+        data.split(separator: "\n").map({String($0)})
     }
-  }
+    
+    let maxRed = 12
+    let maxGreen = 13
+    let maxBlue = 14
 
   // Replace this with your solution for the first part of the day's challenge.
   func part1() -> Any {
-    // Calculate the sum of the first set of input data
-    entities.first?.reduce(0, +) ?? 0
+      var validGameIds: [Int] = []
+      entities.forEach({ line in
+          if !line.isEmpty {
+              let game = isGameValid(game: line)
+              if game.isValid {
+                  validGameIds.append(game.id)
+              }
+          }
+      })
+      return validGameIds.reduce(0, +)
   }
 
   // Replace this with your solution for the second part of the day's challenge.
   func part2() -> Any {
-    // Sum the maximum entries in each set of data
-    entities.map { $0.max() ?? 0 }.reduce(0, +)
+      var validGameIds: [Int] = []
+      entities.forEach({ line in
+          if !line.isEmpty {
+              let game = isGameValid(game: line)
+              validGameIds.append(game.power)
+          }
+      })
+      return validGameIds.reduce(0, +)
   }
+}
+
+extension Day02 {
+    func isGameValid(game: String) -> Game {
+        let id = game.getGameId()
+        var isValid = true
+        var minRed = 0
+        var minGreen = 0
+        var minBlue = 0
+        
+        
+        let handfuls = game.getArrayOfHandfuls()
+        handfuls.forEach({ handful in
+            let draws = handful.components(separatedBy: ",")
+            draws.forEach({ draw in
+                if draw.contains("red") {
+                    let currentRed = Int(draw.filter("0123456789".contains))!
+                    if currentRed > minRed {
+                        minRed = currentRed
+                    }
+                    if currentRed > maxRed {
+                        isValid = false
+                    }
+                } else if draw.contains("green") {
+                    let currentGreen = Int(draw.filter("0123456789".contains))!
+                    if  currentGreen > minGreen {
+                        minGreen = currentGreen
+                    }
+                    if currentGreen > maxGreen {
+                        isValid = false
+                    }
+                } else if draw.contains("blue") {
+                    let currentBlue = Int(draw.filter("0123456789".contains))!
+                    if
+                       currentBlue > minBlue {
+                        minBlue = currentBlue
+                    }
+                    if currentBlue > maxBlue {
+                        isValid = false
+                    }
+                }
+
+                })
+        })
+        
+        let power = minRed * minGreen * minBlue
+        
+        return Game(id: id, isValid: isValid, minRed: minRed, minGreen: minGreen, minBlue: minBlue, power: power)
+    }
+}
+
+extension String {
+    func getGameId() -> Int {
+        let noGame = self.dropFirst("Game ".count)
+        let splitString = noGame.components(separatedBy: ":")
+        return Int(String(splitString.first!))!
+    }
+    
+    func getArrayOfHandfuls() -> [String] {
+        let colonSplit = self.components(separatedBy: ":")
+        let handfulSplit = colonSplit.last!.components(separatedBy: ";")
+        return handfulSplit
+    }
 }
--- Tests/Day02.swift
+++ Tests/Day02.swift
@@ -7,30 +7,20 @@ import XCTest
 final class Day02Tests: XCTestCase {
   // Smoke test data provided in the challenge question
   let testData = """
-    1000
-    2000
-    3000
-
-    4000
-
-    5000
-    6000
-
-    7000
-    8000
-    9000
-
-    10000
-
+    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
     """
 
   func testPart1() throws {
-    let challenge = Day00(data: testData)
-    XCTAssertEqual(String(describing: challenge.part1()), "6000")
+    let challenge = Day02(data: testData)
+    XCTAssertEqual(String(describing: challenge.part1()), "8")
   }
 
   func testPart2() throws {
-    let challenge = Day00(data: testData)
-    XCTAssertEqual(String(describing: challenge.part2()), "32000")
+    let challenge = Day02(data: testData)
+    XCTAssertEqual(String(describing: challenge.part2()), "2286")
   }
 }
